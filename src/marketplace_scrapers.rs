91030cf4a9f68bd4a002c8edc46d5bd8d55411aa
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-07-21 00:17:27 +0700

    parse_document() function

--- src/marketplace_scrapers.rs
+++ src/marketplace_scrapers.rs
@@ -1,4 +1,5 @@
 use crate::models::Product;
+use scraper::Html;
 
 pub struct Tokopedia;
 pub struct Shopee;
@@ -6,28 +7,45 @@ pub struct Blibli;
 pub struct Bukalapak;
 
 pub trait MarketplaceScraper {
+    fn parse_document() -> Html;
     fn get_products() -> Vec<Product>;
 }
 
 impl MarketplaceScraper for Tokopedia {
+    fn parse_document() -> Html {
+        todo!()
+    }
+
     fn get_products() -> Vec<Product> {
         todo!()
     }
 }
 
 impl MarketplaceScraper for Shopee {
+    fn parse_document() -> Html {
+        todo!()
+    }
+
     fn get_products() -> Vec<Product> {
         todo!()
     }
 }
 
 impl MarketplaceScraper for Blibli {
+    fn parse_document() -> Html {
+        todo!()
+    }
+
     fn get_products() -> Vec<Product> {
         todo!()
     }
 }
 
 impl MarketplaceScraper for Bukalapak {
+    fn parse_document() -> Html {
+        todo!()
+    }
+
     fn get_products() -> Vec<Product> {
         todo!()
     }
