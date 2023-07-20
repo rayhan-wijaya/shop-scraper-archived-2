3c73107231e6dbb980e2bf2e524881e3f7b4c59b
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-07-21 00:25:34 +0700

    used init_routes in main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use shop_scraper::routes::init_routes;
+
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    let app = tide::new();
+    let mut app = tide::new();
+    init_routes(&mut app);
+
     app.listen("127.0.0.1:8080").await?;
 
     Ok(())
