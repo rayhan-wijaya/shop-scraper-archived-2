56412963852d2caf5765e8b8c54fa1f063ff8d25
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-07-28 19:27:40 +0700

    changed image_url to Option<String>

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,7 @@
 pub struct Product {
     pub id: i32,
     pub name: String,
-    pub image_url: String,
+    pub image_url: Option<String>,
     pub url: String,
     pub rating: Option<f32>,
     pub price_in_idr: f32,
