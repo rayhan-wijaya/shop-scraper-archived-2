b94eddaec19c16ae14fc540e6483c3b943409457
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-08-02 20:09:09 +0700

    removed lifetime parameters from Bukalapak functions

--- src/marketplace_scrapers.rs
+++ src/marketplace_scrapers.rs
@@ -184,11 +184,11 @@ impl MarketplaceScraper for Blibli {
 }
 
 impl MarketplaceScraper for Bukalapak {
-    fn parse_document<'a>(search_query: String) -> Result<Html, ScrapingError<'a>> {
+    fn parse_document(search_query: String) -> Result<Html, ScrapingError> {
         todo!()
     }
 
-    fn get_cheap_products<'a>(search_query: String) -> Result<Vec<Product>, ScrapingError<'a>> {
+    fn get_cheap_products(search_query: String) -> Result<Vec<Product>, ScrapingError> {
         let document = Self::parse_document(search_query)?;
 
         todo!()
